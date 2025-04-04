29439fb33bf677d97055085c9b469cf528261381
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-04-04 22:55:45 +0200

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -13,7 +13,8 @@ app.use(express.json());
 // Connexion à MongoDB en utilisant l'URI stockée dans les variables d'environnement
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connecté à MongoDB"))
-  .catch((error) => console.error("Erreur de connexion MongoDB : ", error));
+  .catch((error) => {
+    console.error("Erreur de connexion MongoDB : ", error);
     process.exit(1);  // Arrêter le serveur en cas de problème de connexion
   });
 
